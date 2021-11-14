fab8db1653a943d04624d3b3176e3d630ddb1571
Zioyi <newsunstudy@163.com>
2021-11-14 13:22:06 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import requests
+import time
+import random
+
+url = "https://api.juejin.cn/content_api/v1/article/detail"
+data = {
+    "article_id": "7022474622901682213"
+}
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+}
+
+cnt = 0
+msg = [
+    "<%d,%d>########[%d]",
+    "####<%d,%d>####[%d]",
+    "########<%d,%d>[%d]",
+]
+for i in range(50):
+    res = requests.post(url, json=data, headers=headers)
+    # print(res.status_code, res.json())
+    block = random.randint(1, 3)
+    time.sleep(block)
+    cnt += 1
+    print(msg[cnt % 3] % (cnt % 10, block, cnt), end="\r")
